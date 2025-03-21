4df052e8899e9ae1ffa654db680e87ef1126e3e9
a2410034 <a2410034@institutmontilivi.cat>
2025-03-21 20:33:16 +0100

    Joc pedra, paper, tisores

---
+++ joc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html> 
+<html lang="ca">
+    <head>
+        <title>PEDRA,PAPER O TISORES</title>
+    </head>
+    <body>
+        <h1>JUGUEM AL PEDRA, PAPER O TISORES</h1>
+
+        <form  name="joc" action="resultat.php" method="GET">           
+            <fieldset>
+             Quina opcio vos? 
+             <select name="opcio">
+                <option value="pedra">Pedra</option> 
+                <option value="paper">Paper</option> 
+                <option value="tisora">Tissores</option>
+             </select><br><br>          
+          
+             <input type="submit" value="Enviar">
+             </fieldset>  
+        </form>
+    </body>
+</html>
---
+++ resultat.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Pedra,paper o tisores</title>
+</head>
+<body>
+
+<?php
+
+$usuari = $_GET["opcio"];
+echo "Has escollit ". $usuari;
+
+echo "<br><br>";
+
+
+$maquina = array("pedra", "paper", "tisores");
+$aleatori = rand(0,2);
+$opcio_maquina=$maquina[$aleatori];
+
+echo "La màquina ha escollit ". $opcio_maquina . "<br><br>";
+
+if($usuari = "pedra" && $opcio_maquina = "pedra")
+{
+echo "Empat";
+}
+
+elseif($usuari = "pedra" && $opcio_maquina = "paper")
+{
+    echo "Guanya la maquina";
+}
+
+elseif($usuari = "pedra" && $opcio_maquina = "tisora")
+{
+    echo "Guanya l'usuari";
+}
+
+elseif($usuari = "paper" && $opcio_maquina = "pedra")
+{
+    echo "Guanya l'usuari";
+}
+
+elseif($usuari = "paper" && $opcio_maquina = "Paper")
+{
+    echo "Empat";
+}
+
+elseif($usuari = "paper" && $opcio_maquina = "tisora")
+{
+    echo "Guanya la maquina";
+}
+
+elseif($usuari = "tisora" && $opcio_maquina = "pedra")
+{
+    echo "Guanya la maquina";
+}
+
+elseif($usuari = "tisora" && $opcio_maquina = "tisora")
+{
+    echo "Empat";
+}
+
+elseif($usuari = "tisora" && $opcio_maquina = "paper")
+{
+    echo "Guanya l'usuari";
+}
+
+ ?> 
+
+</body>
+</html>
